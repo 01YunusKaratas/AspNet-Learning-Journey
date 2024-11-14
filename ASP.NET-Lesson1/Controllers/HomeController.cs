dbf18a1f9642b80486e825a05cdbd5e101757db5
YUNUS EMRE KARATAS <yunuskaratas589@gmail.com>
2024-11-14 11:39:32 +0300

    ASP.NET-1
    Bu derste ASP.NET konusuna giriş yaptık.

---
+++ ASP.NET-Lesson1/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+using ASP.NET_Lesson1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ASP.NET_Lesson1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string name = "yunus emre";
+
+            ViewData["name"]=name;
+
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
