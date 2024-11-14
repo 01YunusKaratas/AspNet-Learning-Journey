8508428d748cb7f011b769f8dcb42c3ebe943c82
YUNUS EMRE KARATAS <yunuskaratas589@gmail.com>
2024-11-14 12:36:16 +0300

    ASP.NET-Lesson2
    In this lesson , We examined 4 ActıonResult type .
    IActionResult,JsonResult,EmptyResult,ContentResult

---
+++ ASP.NET-Lesson2/Controllers/OrnekController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Lesson2.Controllers
+{
+    public class OrnekController : Controller
+    {
+        public IActionResult Index()
+        {
+
+
+            return View();
+        }
+
+        public IActionResult ContentResult()
+        {
+
+            return Content("Content Result");
+        }
+
+
+        public IActionResult JsonResult()
+        {
+
+            return Json(new { id = 1, name = "yunus", school = "MCBÜ" });
+        }
+
+        public IActionResult EmptyResult()
+        {
+            return new EmptyResult();//hiçbirşey döndürmez.
+
+        }
+    }
+}
---
+++ ASP.NET-Lesson2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ASP.NET_Lesson2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
