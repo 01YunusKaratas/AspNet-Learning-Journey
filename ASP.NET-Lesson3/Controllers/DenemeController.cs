62fac5be3b2af4a64ee26b7019310c830a52c8c5
YUNUS EMRE KARATAS <yunuskaratas589@gmail.com>
2024-11-14 14:24:03 +0300

    RedırectToAction
    learned RedirectToAction method for data transfer between two page.

---
+++ ASP.NET-Lesson3/Controllers/DenemeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_Lesson3.Controllers
+{
+    public class DenemeController : Controller
+    {
+        public IActionResult Index()
+        {
+          
+            return RedirectToAction("Index1", "Deneme", new {name ="yunus emre karataş"});
+        }
+
+        
+        public IActionResult Index1(string name)
+        {
+
+
+            ViewBag.Name = name;  // name parametresini ViewBag ile view'a gönderiyoruz
+            return View();
+        }
+    }
+}
